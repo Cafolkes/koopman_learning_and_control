8abb1f796674166a9a8d2386af639871de7243ca
Victor Dorobantu <victor.d.dorobantu@gmail.com>
2019-08-21 13:55:50 -0700

    Fixed bug in cart pole dynamics and updated to ConfigurationDynamics in example

--- core/systems/cart_pole.py
+++ core/systems/cart_pole.py
@@ -1,5 +1,4 @@
 from numpy import array, concatenate, cos, dot, reshape, sin, zeros
-from numpy.linalg import solve
 
 from core.dynamics import RoboticDynamics
 
@@ -7,26 +6,27 @@ class CartPole(RoboticDynamics):
     def __init__(self, m_c, m_p, l, g=9.81):
         RoboticDynamics.__init__(self, 2, 1)
         self.params = m_c, m_p, l, g
-
+    
     def D(self, q):
         m_c, m_p, l, _ = self.params
         _, theta = q
         return array([[m_c + m_p, m_p * l * cos(theta)], [m_p * l * cos(theta), m_p * (l ** 2)]])
-
+    
     def C(self, q, q_dot):
         _, m_p, l, _ = self.params
         _, theta = q
-        return array([[0, -m_p * l * sin(theta)], [0, 0]])
-
-    def G(self, q):
-        _, m_p, l, g = self.params
-        _, theta = q
-        return array([0, -m_p * g * l * sin(theta)])
-
+        _, theta_dot = q_dot
+        return array([[0, -m_p * l * theta_dot * sin(theta)], [0, 0]])
+    
     def U(self, q):
         _, m_p, l, g = self.params
         _, theta = q
         return m_p * g * l * cos(theta)
-
+    
+    def G(self, q):
+        _, m_p, l, g = self.params
+        _, theta = q
+        return array([0, -m_p * g * l * sin(theta)])
+    
     def B(self, q):
         return array([[1], [0]])
