2b6b229f2707cc7f473dfd7abc17552f8ff5fb95
Cafolkes <carl.axel.folkestad@gmail.com>
2020-05-07 15:19:14 +0200

    Merged in files from keedmd

--- koopman_core/systems/__init__.py
+++ koopman_core/systems/__init__.py
@@ -1,4 +1,5 @@
-from .cart_pole import CartPole
 from .double_inverted_pendulum import DoubleInvertedPendulum
 from .inverted_pendulum import InvertedPendulum
-from .planar_quadrotor import PlanarQuadrotor
+from .cart_pole import CartPole
+from .closed_koopman_sys import ClosedSubspaceSys
+from .one_dim_drone import OneDimDrone
--- systems/__init__.py
+++
@@ -1,5 +0,0 @@
-from .double_inverted_pendulum import DoubleInvertedPendulum
-from .inverted_pendulum import InvertedPendulum
-from .cart_pole import CartPole
-from .closed_koopman_sys import ClosedSubspaceSys
-from .one_dim_drone import OneDimDrone
