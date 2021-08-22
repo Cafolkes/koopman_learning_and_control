07abad7c93eb81ddf122b3be2aaf72f26b43d295
Carl Folkestad <carl.folkestad@caltech.edu>
2021-08-22 16:49:42 -0700

    Import changes

--- koopman_core/learning/__init__.py
+++ koopman_core/learning/__init__.py
@@ -1,7 +1,7 @@
 from .edmd import Edmd
 from .bilinear_edmd import BilinearEdmd
 from .edmd_aut import Edmd_aut
-from .lasso_qp import LassoQp
+#from .lasso_qp import LassoQp
 from .fl_bilinear_learner import FlBilinearLearner
 from .utils import plot_trajectory, plot_trajectory_ep, differentiate_vec, differentiate
 from .koopman_net import KoopmanNet
